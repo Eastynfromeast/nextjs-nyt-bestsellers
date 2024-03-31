eaaf0319bae14649580e1630ef65bee1c72127ba
Eastyn <eastynwork@gmail.com>
2024-03-31 18:06:54 +0900

    why it does not work

---
+++ app/(home)/loading.tsx
@@ -0,0 +1,9 @@
+import styles from "../../styles/common.module.css";
+
+export default function LoadingHome() {
+	return (
+		<div className={styles.container}>
+			<h2>We are heading to home</h2>
+		</div>
+	);
+}
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,6 @@
 import { getBookLists } from "../app/(home)/page";
 import styles from "../styles/common.module.css";
+import { getCategorizedBooks } from "./booklist";
 
 export default async function Footer() {
 	const data = await getBookLists();
